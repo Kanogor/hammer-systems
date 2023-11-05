e9b0e2e7d4f2db602c982d57f48fda27e62f3fe0
Kanogor <the.kanogor@yandex.ru>
2023-11-05 10:45:18 +0300

    chips done

--- app/src/main/java/ru/kanogor/hammersystems/presentation/menu/MenuFragment.kt
+++ app/src/main/java/ru/kanogor/hammersystems/presentation/menu/MenuFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import ru.kanogor.hammersystems.databinding.FragmentMenuBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.kanogor.hammersystems.R
 
 class MenuFragment : Fragment() {
 
@@ -37,6 +39,22 @@ class MenuFragment : Fragment() {
             pizzaAdapter.submitList(it)
         }.launchIn(viewLifecycleOwner.lifecycleScope)
 
+        binding.chipGroup.setOnCheckedStateChangeListener { group, _ ->
+
+            when (group.checkedChipId) {
+                R.id.pizza_chip -> {
+                    viewModel.pizzas.onEach {
+                        pizzaAdapter.submitList(it)
+                    }.launchIn(viewLifecycleOwner.lifecycleScope)
+                }
+                else -> {
+                    pizzaAdapter.submitList(emptyList())
+                    Toast.makeText(requireContext(), "Товары отсутствуют", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+        }
 
     }
 
